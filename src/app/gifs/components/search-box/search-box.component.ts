72c2ab4ce2d0b52ceb64e2bb6d78d07496f356b1
“Juliana <jgrajales277@gmail.com>
2024-03-20 17:24:38 -0500

    creation of GifsService

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+
+
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
   template: `
-    <h5>Search</h5>
-    <input type="text" class="form-control" placeholder="Search for gifs" />
-  `,
+    <h5>Search:</h5>
+    <input type="text"
+      class="form-control"
+      placeholder="Search gifs..."
+      (keyup.enter)="searchTag()"
+      #txtTagInput
+    >
+  `
 })
+
 export class SearchBoxComponent  {
-  constructor() {}
+
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+
+  constructor(private gifsService: GifsService) { }
+
+
+
+  // searchTag( newTag: string ) {
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+    this.gifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value= ''
+    console.log({newTag})
+
+  }
+
 }
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../../../gifs/components/services/gifs.service';
 
 @Component({
   selector: 'shared-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
+  constructor(private GifsService: GifsService) {}
+
+  get tags() {
+    return this.GifsService.tagsHistory;
+  }
+
 
 }
