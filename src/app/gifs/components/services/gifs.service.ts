e13530f17f95c316af9f706bc3eabb1251056a07
“Juliana <jgrajales277@gmail.com>
2024-06-14 19:45:19 -0500

    Mostrar las imagenes delos gifs y almacenarlas en el Local Storage del navegador

--- src/app/gifs/components/services/gifs.service.ts
+++ src/app/gifs/components/services/gifs.service.ts
@@ -11,7 +11,10 @@ export class GifsService {
   private apiKey:       string = '7D5R6tw1Z3NQcTkUarmEoVRC235JGdkv'
   private serviceUrl:   string = 'https://api.giphy.com/v1/gifs'
 
-  constructor( private http: HttpClient) {}
+  constructor( private http: HttpClient) {
+      this.loadLocalStorage();
+      console.log('Gifs Service Ready');
+  }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -26,9 +29,22 @@ export class GifsService {
 
      this._tagsHistory.unshift(tag);
      this._tagsHistory = this.tagsHistory.splice(0,10);
-
+     this.saveLocalStorage();
   }
 
+  private saveLocalStorage(): void {
+
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage(): void {
+    if(!localStorage.getItem('history')) return;
+    // If there isn't any data in the key 'history' do nothing (return)
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+    if(this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0]);
+
+  }
    searchTag(tag: string):void {
 
     if (tag.length === 0) return;
